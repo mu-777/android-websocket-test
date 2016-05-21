01d0cd5a155eb8e802705d98e82202c0aee2eda5
RyosukeMurata <ryosuke.murata.7@gmail.com>
2016-05-21 20:34:15 +0900

    update

--- app/src/main/java/com/mu_777/websocket/MainActivity.java
+++ app/src/main/java/com/mu_777/websocket/MainActivity.java
@@ -6,19 +6,61 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.apache.http.message.BasicNameValuePair;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends Activity {
 
 
     private static final String TAG = "MainActivity";
-    MainView mainView;
+    TextView statusTextView;
+    List<BasicNameValuePair> extraHeaders = Arrays.asList(
+            new BasicNameValuePair("Cookie", "session=abcd")
+    );
+    WebSocketClient client;
+    WebSocketClient.Listener wsListener = new WebSocketClient.Listener() {
+        @Override
+        public void onConnect() {
+//            statusTextView.setText("Connected!");
+            Log.d(TAG, "Connected!");
+        }
+
+        @Override
+        public void onMessage(String message) {
+            Log.d(TAG, String.format("Got string message! %s", message));
+        }
+
+        @Override
+        public void onMessage(byte[] data) {
+            Log.d(TAG, String.format("Got binary message! %s", new String(data)));
+        }
+
+        @Override
+        public void onDisconnect(int code, String reason) {
+//            statusTextView.setText("Disconnected!");
+            Log.d(TAG, String.format("Disconnected! Code: %d Reason: %s", code, reason));
+        }
+
+        @Override
+        public void onError(Exception error) {
+//            statusTextView.setText("Error!");
+            Log.e(TAG, "Error!", error);
+        }
+    };
+
+    //    WebSocketClient client = new WebSocketClient(URI.create("ws://0.0.0.0:3000"),
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mainView = (MainView) findViewById(R.id.mainView);
+        statusTextView = (TextView) findViewById(R.id.textView_status);
 
         Button connectButton = (Button) findViewById(R.id.btn_connect);
         connectButton.setOnClickListener(new View.OnClickListener() {
@@ -27,25 +69,31 @@ public class MainActivity extends Activity {
                 EditText ipEditText = (EditText) findViewById(R.id.editText_serverip);
                 EditText portEditText = (EditText) findViewById(R.id.editText_serverport);
                 String address = "ws://" + ipEditText.getText().toString() + ":" + portEditText.getText().toString();
-                Log.d(TAG, String.format("Address: %s", address));
-                mainView.wsConnect(address);
+                Log.e(TAG, String.format("Address: %s", address));
+                if (client != null) {
+                    client.disconnect();
+                    client = null;
+                }
+                client = new WebSocketClient(URI.create(address), wsListener, extraHeaders);
+                client.connect();
             }
         });
 
+        // ëóêMÉ{É^Éì
         Button sendButton = (Button) findViewById(R.id.btn_send);
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 EditText msgEditText = (EditText) findViewById(R.id.editText_msg);
-                String msg = msgEditText.getText().toString();
-                mainView.wsSendMsg(msg);
+                client.send(msgEditText.getText().toString());
             }
         });
+
     }
 
     @Override
     protected void onDestroy() {
-        mainView.wsDisconnect();
+        client.disconnect();
         super.onDestroy();
     }
 }
